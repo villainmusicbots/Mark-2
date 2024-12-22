09c568ecad03c45ebb540452bc5cc0361395a3cb
PROOTAKU1 <Prootaku4141@gmail.com>
2024-12-22 17:59:18 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -4,14 +4,14 @@ class Config(object):
     # Get this value from my.telegram.org/apps
     OWNER_ID = "5909658683"
     sudo_users = "5909658683", "8019277081", "5608779258", "6961368696", "1881562083", "8035449599"
-    GROUP_ID = -1002311769574
-    TOKEN = "8105814063:AAGWz48XsphzWGqja2Y4AxhkQzJYUOxPJn8"
-    mongo_url = "mongodb+srv://shadowxfamilly:<Prootaku999S>@cluster0.sf5xk.mongodb.net/"
+    GROUP_ID = -1002339477315
+    TOKEN = "8181626432:AAEwT0nmQ5ivF52zpVmDHYXgpChp-M5BQwk"
+    mongo_url = "mongodb+srv://TEAMBABY01:UTTAMRATHORE09@cluster0.vmjl9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     PHOTO_URL = ["https://files.catbox.moe/wy70cl.jpg", "https://files.catbox.moe/wy70cl.jpg"]
     SUPPORT_CHAT = "WH_SUPPORT_GC"
     UPDATE_CHAT = "iamvillain77"
     BOT_USERNAME = "@Random_test_009_bot"
-    CHARA_CHANNEL_ID = "-1002311769574"
+    CHARA_CHANNEL_ID = "-1002339477315"
     api_id = "24061032"
     api_hash = "5ad029547f2eeb5a0b68b05d0db713be"
 
